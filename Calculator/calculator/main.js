5cfbc319e3dcdff3dbcc4933cb2f9c91d140710b
Kilian <96377170+kilianmendez@users.noreply.github.com>
2023-09-25 18:47:11 +0200

    Calculator project
    First commit. Started basic html and css

---
+++ Calculator/calculator/main.js
@@ -0,0 +1,21 @@
+let btn0 = document.getElementById("n0")
+let btn1 = document.getElementById("n1");
+let btn2 = document.getElementById("n2");
+let btn3 = document.getElementById("n3");
+let btn4 = document.getElementById("n4");
+let btn5 = document.getElementById("n5");
+let btn6 = document.getElementById("n6");
+let btn7 = document.getElementById("n7");
+let btn8 = document.getElementById("n8");
+let btn9 = document.getElementById("n9");
+
+let btnDivide = document.getElementById("divide")
+let btnTimes = document.getElementById("times")
+let btnMinus = document.getElementById("minus")
+let btnPlus = document.getElementById("plus")
+let btnEqual = document.getElementById("equal")
+let btnPoint = document.getElementById("point")
+
+let buttons = document.querySelector("button")
+
+buttons.addEventListener("click")
